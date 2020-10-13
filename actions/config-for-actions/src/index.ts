6bf3c788cc31dde91927e88a3c0921051f900260
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-12 22:48:33 -0700

    chore: test drive

--- actions/config-for-actions/src/index.ts
+++ actions/config-for-actions/src/index.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as glob from '@actions/glob';
+import fs from 'fs';
 
 const main = async (): Promise<void> => {
     const globOptions = {
@@ -7,8 +8,19 @@ const main = async (): Promise<void> => {
     }
     const configFiles = core.getInput('config_files');
     const globber = glob.create(configFiles, globOptions);
+    const globalConfig: Record<string, string> = {};
     for await (const file of (await globber).globGenerator()) {
-        console.log(file)
+        core.info(`Use ${file} as a configuration file.`);
+        const configContent = fs.readFileSync(file, 'utf8');
+        const parsedConfig: Record<string, string> = JSON.parse(configContent);
+        for (const configName in parsedConfig) {
+            globalConfig[configName] = parsedConfig[configName];
+        }
+    }
+    for (const configName in globalConfig) {
+        const configValue = globalConfig[configName];
+        core.info(`Set ${configName} to ${configValue}.`);
+        core.setOutput(configName, configValue);
     }
 }
 
