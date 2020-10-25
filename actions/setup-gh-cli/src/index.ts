f5280b04fa099ac17125bc710394d5d6c3273389
Tianhao Zhou <tianhaoz@umich.edu>
2020-10-24 23:19:49 -0700

    fix

--- actions/setup-gh-cli/src/index.ts
+++ actions/setup-gh-cli/src/index.ts
@@ -1,26 +1,37 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import path from 'path';
+import fs from 'fs';
 
-let myOutput = '';
-let myError = '';
+let infoOutput = '';
+let errorOutput = '';
 
 const options: exec.ExecOptions = {
     listeners: {
         stdout: (data: Buffer) => {
-            myOutput += data.toString();
+            infoOutput += data.toString();
         },
         stderr: (data: Buffer) => {
-            myError += data.toString();
+            errorOutput += data.toString();
         }
     },
 };
 
 const main = async (): Promise<void> => {
-    if (!process.env['GITHUB_TOKEN']) {
-        core.error('GITHUB_TOKEN not found.');
+    const token = core.getInput('token');
+    const home = process.env['HOME'];
+    if (token === undefined || home === undefined) {
+        core.error('Token not found.');
     } else {
-        await exec.exec('gh', ['auth', 'login'], options);
+        const tokenDir = path.join(home, 'temp', 'setup_gh_cli');
+        await io.mkdirP(tokenDir);
+        const tokenFile = path.join(tokenDir, 'token.txt');
+        fs.writeFileSync(tokenFile, token);
+        await exec.exec(`gh auth login --with-token ${tokenFile}`, undefined, options);
     }
+    core.info(`Standard output: ${infoOutput}`);
+    core.info(`Error output: ${errorOutput}`);
 }
 
 main();
